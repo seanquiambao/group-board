367706f65caee79061f88fd08266a803deab8eb0
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-04-14 00:20:08 -0700

    landing page setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <>Hello World</>;
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen gap-y-5">
+      <div className="text-7xl text-board-black font-semibold">GroupBoard</div>
+      <div className="text-4xl text-board-black">
+        whiteboard together all in one web app
+      </div>
+      <Link
+        className="bg-board-blue-200 text-white text-2xl font-semibold py-2 px-7 rounded-2xl hover:brightness-110 duration-300"
+        href="/dashboard"
+      >
+        sign into google
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
