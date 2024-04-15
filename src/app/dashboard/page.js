dba059284653aa2378e51218e1d14bd40cac7f03
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-04-14 23:30:31 -0700

    auth setup

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,21 +1,24 @@
+"use client";
 import Image from "next/image";
 import Card from "@/components/dashboard/Card";
 import ProtectedPage from "@/components/ProtectedPage";
+import { useSession } from "next-auth/react";
 
 const Page = () => {
+  const { data: session } = useSession();
   return (
     <ProtectedPage className="flex-1">
-      <div className="flex flex-col items-start justify-center ml-[10%] w-full h-2/3 gap-y-6">
-        <div className="flex flex-row items-center  w-1/2 gap-x-12">
+      <div className="flex flex-col items-start justify-center ml-[10%] h-2/3 gap-y-6">
+        <div className="flex flex-row items-center w-1/2 gap-x-12">
           <Image
-            src="/temp.png"
-            alt="Profile Picture"
+            src={session.user.image}
+            alt={`${session.user.name}'s profile picture`}
             width={200}
             height={200}
             className="border-4 rounded-full border-board-blue-200"
           />
           <div className="text-6xl text-black font-semibold">
-            Hello, toastmeal!
+            Hello, {session.user.name}!
           </div>
         </div>
         <div className="text-black text-4xl font-semibold">Your Rooms</div>
