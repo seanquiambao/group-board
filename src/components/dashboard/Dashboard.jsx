26da96e0069a4f5d1d2bfa011115a9310903f5a5
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-05-24 02:08:54 -0700

    remove extra tag

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -60,42 +60,38 @@ const Dashboard = () => {
   };
 
   return (
-    <>
+    <div className="flex flex-col items-start justify-center ml-[10%] h-2/3 gap-y-6">
       {popup.visible && (
         <Popup popup={popup} setPopup={setPopup} onClick={popup.onClick} />
       )}
-      <div className="flex flex-col items-start justify-center ml-[10%] h-2/3 gap-y-6">
-        <div className="flex flex-row items-center w-1/2 gap-x-12">
-          <Image
-            src={session.user.image}
-            alt={`${session.user.name}'s profile picture`}
-            width={200}
-            height={200}
-            className="border-4 rounded-full border-board-blue-200"
-          />
-          <div className="text-6xl text-black font-semibold">
-            Hello, {session.user.name}!
-          </div>
+      <div className="flex flex-row items-center w-1/2 gap-x-12">
+        <Image
+          src={session.user.image}
+          alt={`${session.user.name}'s profile picture`}
+          width={200}
+          height={200}
+          className="border-4 rounded-full border-board-blue-200"
+        />
+        <div className="text-6xl text-black font-semibold">
+          Hello, {session.user.name}!
         </div>
-        <div className="text-black text-4xl font-semibold">Your Rooms</div>
-        {error && <div>Something went wrong</div>}
-        {!data && <div>Loading</div>}
-        {data && (
-          <div className="flex flex-row gap-x-4 w-full items-center">
-            {data.items.rooms?.map((room, index) => (
-              <Card
-                key={index}
-                id={room}
-                handleDelete={() => confirmDelete(room)}
-              />
-            ))}
-            {data.items.rooms.length < 3 && (
-              <Create handleClick={handleCreate} />
-            )}
-          </div>
-        )}
       </div>
-    </>
+      <div className="text-black text-4xl font-semibold">Your Rooms</div>
+      {error && <div>Something went wrong</div>}
+      {!data && <div>Loading</div>}
+      {data && (
+        <div className="flex flex-row gap-x-4 w-full items-center">
+          {data.items.rooms?.map((room, index) => (
+            <Card
+              key={index}
+              id={room}
+              handleDelete={() => confirmDelete(room)}
+            />
+          ))}
+          {data.items.rooms.length < 3 && <Create handleClick={handleCreate} />}
+        </div>
+      )}
+    </div>
   );
 };
 
